cd82347f61f061509d2485bd493e95b6bfab6594
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-09-02 13:32:58 -0400

    Began my_uniq method

---
+++ spec/rspec_spec.rb
@@ -0,0 +1,10 @@
+
+
+# describe Rspec do
+    describe "#my_uniq" do
+        subject(:uniq_arr) { uniq_arr = [1, 2, 1, 3, 3] }
+        it "return an array with only unique values" do
+            
+        end
+    end
+end
